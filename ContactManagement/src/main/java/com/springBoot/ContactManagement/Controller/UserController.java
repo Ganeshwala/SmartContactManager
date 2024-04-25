23d9fe32f76b21355df9581fcb6dd2231415cfc5
ganeshwala <ganeshwalachintan@gmail.com>
2024-04-24 18:22:09 -0700

    Add Contact Module

--- ContactManagement/src/main/java/com/springBoot/ContactManagement/Controller/UserController.java
+++ ContactManagement/src/main/java/com/springBoot/ContactManagement/Controller/UserController.java
@@ -5,8 +5,11 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springBoot.ContactManagement.Entites.ContactDeatil;
 import com.springBoot.ContactManagement.Entites.User;
 import com.springBoot.ContactManagement.JpaRepository.UserJpaRepository;
 
@@ -17,16 +20,26 @@ public class UserController {
 	@Autowired
 	UserJpaRepository userJpaRepository;
 	
+	//common method which is call every time when this controller is call (kind of default method for all others method)
+	@ModelAttribute
+	private User getUserDetails(Model model,Principal principal) {
+		String loginName = principal.getName();
+		System.out.println("User page"+loginName);
+		User userDetailsByUserName = this.userJpaRepository.getUserDetailsByUserName(loginName);
+		model.addAttribute("User", userDetailsByUserName);
+		return userDetailsByUserName;
+	}
+	
 	@RequestMapping("/dashBoard")
 	public String getUserDashBoard(Model model,Principal principal) {
-		String name = principal.getName();
-		System.out.println("User page"+name);
-		
-		User userDetailsByUserName = this.userJpaRepository.getUserDetailsByUserName(name);
-		
-		model.addAttribute("User", userDetailsByUserName);
-		
 		model.addAttribute("title", "User Loging");
 		return"UserPages/userDashBoard";
+	}
+	
+	@GetMapping("/addContacts")
+	public String addContacts(Model md,Principal principal) {
+		md.addAttribute("title", "Add Contact");
+		md.addAttribute("contactInfo",new ContactDeatil());
+		return "UserPages/addContact";
 	}
 }
